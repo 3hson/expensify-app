78b864149257d181a06e5aa9594dfd95a6c443ae
ehsan <ehsankhakian@hotmail.com>
2018-02-08 15:23:54 +0330

    Setup production build and server

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -55,9 +55,9 @@ export class ExpenseListFilters extends React.Component{
     }
 }
 
-const mapStateToProps= (state)=>{
+const mapStateToProps= (state)=>({
     filters:state.filters
-};
+})
 
 const mapDispatchToProps=()=>({
     setTextFilter: (text)=> dispatch(setTextFilter(text)),
